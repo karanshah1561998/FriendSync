8df0fdb103863d645332bfbef452b98aa41d0d24
Karan Shah <karanshah1561998@gmail.com>
2025-03-08 17:04:45 -0500

    Completed login/logout/updateProfile/signup routes & Middleware

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT: " + PORT);
+    connectDB();
+});
