667785276609f7a1fddda3f6de9fe187357e09de
Karan Shah <karanshah1561998@gmail.com>
2025-03-08 23:31:16 -0500

    Implemented Message Routes and Some fixes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const signup = async (req, res) => {
             res.status(400).json({ message: "Invalid user data" });
         }
     } catch (error) {
-        console.log("Error in Signup Controller", error.message);
+        console.log("Error in Signup Controller: ", error.message);
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
@@ -68,7 +68,7 @@ export const login = async (req, res) => {
             profilePic: user.profilePic,
         });
     } catch (error) {
-        console.log("Error in Login Controller", error.message);
+        console.log("Error in Login Controller: ", error.message);
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
@@ -101,7 +101,7 @@ export const updateProfile = async (req, res) => {
 
         res.status(200).json(updatedUser);
     } catch (error) {
-        console.log("Error in Update Profile Controller", error);
+        console.log("Error in Update Profile Controller: ", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
@@ -110,7 +110,7 @@ export const checkAuth = (req, res) => {
     try {
         res.status(200).json(req.user);
     } catch (error) {
-        console.log("Error in CheckAuth Controller", error.message);
+        console.log("Error in CheckAuth Controller: ", error.message);
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
