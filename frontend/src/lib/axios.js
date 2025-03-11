b3c3100d6b923827847c6d1de59f8359e4f1fe16
Karan Shah <karanshah1561998@gmail.com>
2025-03-11 15:29:29 -0400

    A few changes for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5055/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5055/api" : "/api",
     withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
